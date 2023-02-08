36c346d377ac82e888faa0ee2e2b851129b1e530
Janine Jimenez <81605403+JSJ9@users.noreply.github.com>
2023-02-07 19:20:01 -0600

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,98 @@
+document.getElementById("wrapper").onclick = function () {
+  var className = " " + wrapper.className + " ";
+
+  this.className = ~className.indexOf(" active ")
+    ? className.replace(" active ", " ")
+    : this.className + " active";
+};
+
+
+document.getElementById("wrapper2").onclick = function () {
+  var className = " " + wrapper2.className + " ";
+
+  this.className = ~className.indexOf(" active ")
+    ? className.replace(" active ", " ")
+    : this.className + " active";
+};
+
+
+document.getElementById("wrapper3").onclick = function () {
+  var className = " " + wrapper3.className + " ";
+
+  this.className = ~className.indexOf(" active ")
+    ? className.replace(" active ", " ")
+    : this.className + " active";
+};
+
+
+document.getElementById("wrapper4").onclick = function () {
+  var className = " " + wrapper4.className + " ";
+
+  this.className = ~className.indexOf(" active ")
+    ? className.replace(" active ", " ")
+    : this.className + " active";
+};
+
+document.getElementById("wrapper5").onclick = function () {
+  var className = " " + wrapper5.className + " ";
+
+  this.className = ~className.indexOf(" active ")
+    ? className.replace(" active ", " ")
+    : this.className + " active";
+};
+
+document.getElementById("wrapper6").onclick = function () {
+  var className = " " + wrapper6.className + " ";
+
+  this.className = ~className.indexOf(" active ")
+    ? className.replace(" active ", " ")
+    : this.className + " active";
+};
+
+document.getElementById("wrapper7").onclick = function () {
+  var className = " " + wrapper7.className + " ";
+
+  this.className = ~className.indexOf(" active ")
+    ? className.replace(" active ", " ")
+    : this.className + " active";
+};
+
+document.getElementById("wrapper8").onclick = function () {
+  var className = " " + wrapper8.className + " ";
+
+  this.className = ~className.indexOf(" active ")
+    ? className.replace(" active ", " ")
+    : this.className + " active";
+};
+
+document.getElementById("wrapper9").onclick = function () {
+  var className = " " + wrapper9.className + " ";
+
+  this.className = ~className.indexOf(" active ")
+    ? className.replace(" active ", " ")
+    : this.className + " active";
+};
+
+document.getElementById("wrapper10").onclick = function () {
+  var className = " " + wrapper10.className + " ";
+
+  this.className = ~className.indexOf(" active ")
+    ? className.replace(" active ", " ")
+    : this.className + " active";
+};
+
+document.getElementById("wrapper11").onclick = function () {
+  var className = " " + wrapper11.className + " ";
+
+  this.className = ~className.indexOf(" active ")
+    ? className.replace(" active ", " ")
+    : this.className + " active";
+};
+
+document.getElementById("wrapper12").onclick = function () {
+  var className = " " + wrapper12.className + " ";
+
+  this.className = ~className.indexOf(" active ")
+    ? className.replace(" active ", " ")
+    : this.className + " active";
+};
